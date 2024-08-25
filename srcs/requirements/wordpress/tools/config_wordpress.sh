945c72a977e10e631665a401d2ed81b5ad38642c
fguirama <florian.guiramand@protonmail.com>
2024-08-25 03:26:16 +0200

    update .env variable name

--- srcs/requirements/wordpress/tools/config_wordpress.sh
+++ srcs/requirements/wordpress/tools/config_wordpress.sh
@@ -3,7 +3,7 @@
 sleep 10
 
 if [ ! -f wp-config.php ]; then
-    wp config create	--allow-root --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_USER_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+    wp config create	--allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
     sleep 2
     wp core download --allow-root
