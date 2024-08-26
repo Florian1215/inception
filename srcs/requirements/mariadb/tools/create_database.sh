bac547d219fe95db24a8924e26258deab6dfb2a0
fguirama <florian.guiramand@protonmail.com>
2024-08-26 19:25:20 +0200

    fix create_database.sh

--- srcs/requirements/mariadb/tools/create_database.sh
+++ srcs/requirements/mariadb/tools/create_database.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#todo test if already exist
+if [ -f /var/lib/mysql ]
+then
+	echo "Mariadb already downloaded"
+else
+  chgrp -R mysql /var/lib/mysql
+  chmod -R g+rwx /var/lib/mysql
 
-chgrp -R mysql /var/lib/mysql
-chmod -R g+rwx /var/lib/mysql
+  service mariadb start
 
-service mariadb start
+  mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+  CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
+  GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';
+  ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+  FLUSH PRIVILEGES;"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+  mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+fi
 
 exec "$@"
