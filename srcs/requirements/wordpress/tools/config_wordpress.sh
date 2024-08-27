191500dd4c3254463dabd4fe431269a6e9d01b14
fguirama <florian.guiramand@protonmail.com>
2024-08-27 17:14:43 +0200

    fix database

--- srcs/requirements/wordpress/tools/config_wordpress.sh
+++ srcs/requirements/wordpress/tools/config_wordpress.sh
@@ -2,12 +2,14 @@
 
 sleep 3
 
-if [ ! -f wp-config.php ];
+if [ -f wp-config.php ]
 then
+	echo "Wordpress already installed"
+else
   cd $VOLUME_WP
   /wp-cli.phar core download --allow-root
   /wp-cli.phar config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb
-  /wp-cli.phar core install --allow-root --url="https://fguirama.42.fr" --title=$COMPOSE_PROJECT_NAME --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL
+  /wp-cli.phar core install --allow-root --url="https://fguirama.42.fr" --title=$COMPOSE_PROJECT_NAME --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
   /wp-cli.phar --allow-root option update siteurl "https://fguirama.42.fr"
   /wp-cli.phar --allow-root option update home "https://fguirama.42.fr"
 fi
